fad29714ea57eac9a80e05358bce1c5733846f6d
zhengshaonong <zhengshaonong@wejoydata.com>
2019-10-17 11:51:08 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  // base: '/blog-demo/',
+  title: 'zsn的前端日志',
+  description: '欢迎访问我的前端日志',
+  themeConfig: {
+    repo: "ZSN1024/blog",
+    nav: [
+      {
+        text: "博客",
+        link: "/blog/"
+      },
+      {
+        text: "面试题",
+        link: "/interview/"
+      },
+      {
+        text: "阅读",
+        link: "/book/"
+      },
+    ],
+    sidebar: {
+      "/blog/": [
+        {
+          title: 'Devops',
+          collapsable: false,
+          children: [
+            "FirstBlog",
+            "second"
+          ]
+        }
+      ]
+    }
+  }
+}
